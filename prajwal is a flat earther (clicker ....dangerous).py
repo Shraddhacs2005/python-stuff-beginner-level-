7f9ffa81c35189045f2254d89fa69a523699be94
Shraddha CS <68186427+Shraddhacs2005@users.noreply.github.com>
2023-09-08 20:37:43 +0530

    Update prajwal is a flat earther (clicker ....dangerous).py

--- prajwal is a flat earther (clicker ....dangerous).py
+++ prajwal is a flat earther (clicker ....dangerous).py
@@ -2,3 +2,5 @@ import pyautogui as trump
 import time
 time.sleep(3)
 trump.click (clicks=10000, interval=0.1)
+#dangerous clicker may cause the device to lag or hang 
+#(ps. do not ask me about the file name :D)
