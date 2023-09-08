3c4ce56ebbdf60aa724a18f1566154796cdd6932
Shraddha CS <68186427+Shraddhacs2005@users.noreply.github.com>
2023-09-08 20:22:32 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,48 @@
+print("my world")
+msg = "hello world !"
+print (msg)
+msg = "I am the best in the world"
+print (msg)
+first_name = 'Lee'
+last_name = "Donghae"
+full_name = first_name + ' ' + last_name
+print (full_name)
+day = "04"
+month = "02"
+year = "2005"
+date = day + '-' + month + '-' + year
+print (date)
+msg = "This is the best day ever !!!!!!"
+print (msg)
+bikes = ['pretty', 'nice', 'cute']
+for bike in bikes: 
+   print (bike)
+bikes.append ('beautiful')
+print (bikes)
+for bike in bikes:
+	print (bike)
+day = "15th"
+month = "June"
+year = "The wrekt year for humanity, 2020"
+date = day + "-" + month + "-" + year
+print (date)
+msg = "And here I am coding random stuff here in Python :D"
+print (msg)
+msg = "happy birthday!!!!!"
+print (msg)
+first_name1 = "Jasmine"
+last_name1 = "Park"
+full_name1 = first_name1 + " " + last_name1
+print (full_name1)
+msg = "I am a proud kpop and kdrama fan"
+print (msg)
+lost = full_name1 + " <3 " + full_name
+print (lost)
+day = "22"
+month = "06"
+year = "2020"
+date = day + "-" + month + "-" + year
+print (date)
+msg = "Today was Lee Minho's birthday"
+print (msg)
+msg = ""
