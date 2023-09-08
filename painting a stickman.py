0504cf03a1febd8e46db84cf32c2837a80a7aaea
Shraddha CS <68186427+Shraddhacs2005@users.noreply.github.com>
2023-09-08 20:34:41 +0530

    Add files via upload

---
+++ painting a stickman.py
@@ -0,0 +1,18 @@
+
+import pyautogui
+import time
+time.sleep(6)
+pyautogui.click(430,58)
+pyautogui.moveTo(280,345, duration = 0.75)
+pyautogui.dragTo(480,645)
+pyautogui.click(390,58)
+pyautogui.moveTo(480,480)
+pyautogui.dragTo(780,480)
+pyautogui.moveTo(520,490)
+pyautogui.dragTo(520,610)
+pyautogui.moveTo(520,470)
+pyautogui.dragTo(520,310)
+pyautogui.moveTo(780,480)
+pyautogui.dragTo(800,620)
+pyautogui.moveTo(780,480)
+pyautogui.dragTo(800,320)
